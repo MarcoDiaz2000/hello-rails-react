ab9798788916ff65fca3abd573e22dc5937d355a
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-09-18 17:45:55 -0600

    Add frontend for api

---
+++ app/javascript/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import greetingReducer from "./slices/greetingSlice";
+
+export const store = configureStore({
+  reducer: {
+    greeting: greetingReducer,
+  },
+});
+
+export default store;
